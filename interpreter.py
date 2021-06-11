008a906359160877986362aae1e4571ac6866f46
mroik <mroik@delayed.space>
2021-06-11 15:59:23 +0200

    Changes to how loops work

--- interpreter.py
+++ interpreter.py
@@ -24,13 +24,10 @@ data = [0]
 pos = 0
 skip = False
 i_instr = 0
+loops = []
 
 while(True):
     char = instr[i_instr]
-    if char == ']' and skip:
-        skip = False
-        i_instr += 1
-        continue
     if char == '>':
         if pos == len(data)-1:
             data.append(0)
@@ -50,20 +47,28 @@ while(True):
             data[pos] = 255
     elif char == '.':
         print(chr(data[pos]), end="")
-    elif char == '[':
-        if data[pos] == 0:
-            skip = True
-    elif char == ']':
-        if data[pos] != 0:
-            while(True):
-                i_instr -= 1
-                char = instr[i_instr]
-                if char == '[':
-                    break
     elif char == ',':
         data[pos] = ord(get_char())
         if data[pos] > 255:
             raise ValueError("Brainfuck only stores byte sized data. Limit yourself to ASCII")
+    elif char == '[':
+        if data[pos] != 0:
+            loops.append(i_instr)
+        else:
+            inside = 0
+            while(True):
+                i_instr += 1
+                char = instr[i_instr]
+                if char == '[':
+                    inside += 1
+                elif char == ']':
+                    if inside != 0:
+                        inside -= 1
+                    else:
+                        break
+    elif char == ']':
+        if data[pos] != 0:
+            i_instr = loops.pop() - 1
     i_instr += 1
     if len(instr)-1 == i_instr:
         break
